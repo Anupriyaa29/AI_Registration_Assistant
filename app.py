9d5695bd738e0bd78ed4926744b672e175ed348c
Anu Priya <has161978@example.com>
2026-07-21 16:48:11 +0530

    Commit 4

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json
+import random
+import joblib
+
+from utils.preprocess import preprocess
 
 app = Flask(__name__)
+
+# Load model and vectorizer
+model = joblib.load("model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+# Load intents
+with open("data/intents.json", "r") as file:
+    intents = json.load(file)
+
+
+def get_response(tag):
+    for intent in intents["intents"]:
+        if intent["tag"] == tag:
+            return random.choice(intent["responses"])
+
+    return "Sorry, I don't understand."
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    user_message = request.json["message"]
+
+    processed = preprocess(user_message)
+    processed = " ".join(processed)
+
+    vector = vectorizer.transform([processed])
+
+    prediction = model.predict(vector)[0]
+
+    response = get_response(prediction)
+
+    return jsonify({"response": response})
--- static/script.js
+++ static/script.js
@@ -1,1 +1,37 @@
-console.log("Chatbot Loaded Successfully!");
+async function sendMessage(){
+
+    const input = document.getElementById("user-input");
+
+    const message = input.value;
+
+    if(message==="")
+        return;
+
+    const chatBox=document.getElementById("chat-box");
+
+    chatBox.innerHTML += "<p><b>You:</b> "+message+"</p>";
+
+    const response=await fetch("/chat",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+
+            message:message
+
+        })
+
+    });
+
+    const data=await response.json();
+
+    chatBox.innerHTML += "<p><b>Bot:</b> "+data.response+"</p>";
+
+    input.value="";
+
+    chatBox.scrollTop=chatBox.scrollHeight;
+}
