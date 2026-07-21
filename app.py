0c70d509b9d8a306c9a827b1d614437a13af1265
Anu Priya <has161978@example.com>
2026-07-21 18:45:52 +0530

    Commit 5

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import json
 import random
 import joblib
 
+registration_state = {}
+
+registration_data = {}
+
 from utils.preprocess import preprocess
 
 app = Flask(__name__)
@@ -32,6 +36,48 @@ def home():
 def chat():
 
     user_message = request.json["message"]
+    user_input = request.json["message"]
+    if user_input.lower() in [
+    "register",
+    "registration",
+    "register me",
+    "i want to register",
+    "start registration"]:
+        registration_state["step"] = "name"
+        registration_data.clear()
+        return jsonify({"response": "Great! Let's begin.\nWhat is your full name?"})
+    
+    if registration_state.get("step") == "name":
+        registration_data["name"] = user_input
+        registration_state["step"] = "email"
+        return jsonify({"response": "Please enter your email address."})
+    
+    if registration_state.get("step") == "email":
+        registration_data["email"] = user_input
+        registration_state["step"] = "phone"
+        return jsonify({"response": "Enter your phone number."})
+    
+    if registration_state.get("step") == "phone":
+        registration_data["phone"] = user_input
+        registration_state["step"] = "college"
+        return jsonify({
+        "response": "Which college do you study in?"
+    })
+        
+    if registration_state.get("step") == "college":   
+        registration_data["college"] = user_input
+        registration_state["step"] = "course"
+        return jsonify({"response": "Which course are you pursuing?"})
+    
+    if registration_state.get("step") == "course":
+        registration_data["course"] = user_input
+        registration_state["step"] = None
+        return jsonify({"response": f"Registration Complete!\n\n"
+            f"Name: {registration_data['name']}\n"
+            f"Email: {registration_data['email']}\n"
+            f"Phone: {registration_data['phone']}\n"
+            f"College: {registration_data['college']}\n"
+            f"Course: {registration_data['course']}"})
 
     processed = preprocess(user_message)
     processed = " ".join(processed)
@@ -43,3 +89,6 @@ def chat():
     response = get_response(prediction)
 
     return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
